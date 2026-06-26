edeb806254cea9cd99498879368a383f55ab62a9
Jaouad Sarouti <jaouadsarti@gmail.com>
2026-06-26 16:01:46 -0400

    setters

---
+++ index.js
@@ -0,0 +1,15 @@
+class HashMap{
+    constructor(load_factor, capacity) {
+    this.load_factor = load_factor;
+    this.capacity = capacity;
+    }
+
+    set load_factor(){
+        this.load_factor = 0.75;
+    }
+
+    set capacity (){
+        this.capacity = 16
+    }
+    
+}
